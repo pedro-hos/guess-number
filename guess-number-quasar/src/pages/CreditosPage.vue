714e0e9d42ac714699aec3f24f58041ded99ea6a
Pedro Hos <pedro-hos@outlook.com>
2024-01-28 15:43:30 -0300

    creating pages

---
+++ guess-number-quasar/src/pages/CreditosPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    <div color="">Teste</div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "CreditosPage",
+};
+</script>
--- guess-number-quasar/src/pages/IndexPage.vue
+++ guess-number-quasar/src/pages/IndexPage.vue
@@ -1,17 +1,39 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="q-pa btn-box">
+      <div class="row">
+        <div class="col-auto">
+          <q-btn
+            class="btn"
+            outline
+            rounded
+            color="primary"
+            label="Jogar"
+            size="xl"
+          />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-auto">
+          <q-btn
+            class="btn"
+            outline
+            rounded
+            color="primary"
+            label="Créditos"
+            size="xl"
+            to="/creditos"
+          />
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
