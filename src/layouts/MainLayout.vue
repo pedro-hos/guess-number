34374803be9a83f3abef8405e60420362b4719c5
Pedro Hos <pedro-hos@outlook.com>
2024-02-03 17:00:53 -0300

    toolbar implemented

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,11 +2,39 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title>
-          <q-btn flat color="light" label="Luke & Mike Dognautas" to="/" />
-        </q-toolbar-title>
+        <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+        <q-toolbar-title>Luke & Mike Dognautas</q-toolbar-title>
       </q-toolbar>
     </q-header>
+
+    <q-drawer
+        v-model="drawer"
+        :width="200"
+        :breakpoint="500"
+        bordered
+        style="background-color: #170e39"
+      >
+        <q-scroll-area class="fit">
+          <q-list>
+            <q-toolbar class="bg-primary text-white shadow-2">
+              <q-toolbar-title>Menu</q-toolbar-title>
+            </q-toolbar>
+
+            <template v-for="(menuItem, index) in menuList" :key="index">
+              <q-item :to="menuItem.to" active="false">
+                <q-item-section avatar>
+                  <q-icon :name="menuItem.icon" />
+                </q-item-section>
+                <q-item-section >
+                  {{ menuItem.label }}
+                </q-item-section>
+              </q-item>
+              <q-separator :key="'sep' + index"  v-if="menuItem.separator" />
+            </template>
+
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -17,7 +45,35 @@
 <script>
 import { defineComponent, ref } from "vue";
 
+const menuList = [
+  {
+    icon: 'home',
+    label: 'Home',
+    separator: true,
+    to: {path: '/', exact: true}
+  },
+  {
+    icon: 'send',
+    label: 'Jogar',
+    separator: true,
+    to: {path: "/game", exact: true}
+  },
+  {
+    icon: 'delete',
+    label: 'Créditos',
+    separator: true,
+    to: {path: "/creditos", exact: true}
+  }
+]
+
 export default defineComponent({
   name: "MainLayout",
+
+  setup () {
+    return {
+      drawer: ref(false),
+      menuList
+    }
+  }
 });
 </script>
