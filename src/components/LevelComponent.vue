00b75a61c7b17a8ebf7df37d9c9a68eadce998f8
Pedro Hos <pedro-hos@outlook.com>
2024-02-03 23:32:59 -0300

    level rules

--- src/components/LevelComponent.vue
+++ src/components/LevelComponent.vue
@@ -4,14 +4,14 @@
     <div class="row justify-center" style="margin-top: 10px;">
       <div class="col-6 text-center" style=" border: dashed #d5d656; margin-left: 10px;">
         <span class="text-h6">
-          A sua primeira missão é me ajudar a lembrar a senha para abrir a porta do foguete! Vamos lá?
+          {{text}}
         </span>
       </div>
     </div>
 
     <div class="row">
       <div class="col">
-        <q-img src="/images/game/rocket.png"
+        <q-img :src="`/images/game/levels/${level}/left.png`"
         style="height: 280px; max-width: 300px"/>
       </div>
       <div class="col" style="margin-top: 80px; margin-left: 250px;">
@@ -35,7 +35,7 @@
 
        </div>
       <div class="col">
-        <q-img src="/images/game/luke.png"
+        <q-img :src="`/images/game/levels/${level}/right.png`"
         style="height: 280px; max-width: 200px; margin-left: 80px;"/>
       </div>
     </div>
@@ -43,7 +43,7 @@
 
     <div class="row" style="margin-top: 40px;">
       <div class="col">
-        <q-img src="/images/game/levels/1/footer.png"
+        <q-img :src="`/images/game/levels/${level}/footer.png`"
         style="height: auto; max-width: 100%"/>
       </div>
     </div>
@@ -70,7 +70,42 @@ export default {
     },
   },
   setup(props, context) {
-    const image = ref("");
+
+    const text = ref("");
+
+    let levelMap = new Map();
+
+    levelMap.set("1", {
+      level: 1,
+      text: "A SUA PRIMEIRA MISSÃO É ME AJUDAR A LEMBRAR A SENHA PARA ABRIR A PORTA DO FOGUETE! VAMOS LÁ?"
+    });
+
+    levelMap.set("2", {
+      level: 2,
+      text: "Belo trabalho! agora você precisa me ajudar a ligar o foguete, será que conseguimos adivinhar o código?"
+    });
+
+    levelMap.set("3", {
+      level: 3,
+      text: "Conseguimos! nossa missão agora é encontrar as coordenadas para o planeta tamon, onde o meu irmão mike está."
+    });
+
+    levelMap.set("4", {
+      level: 4,
+      text: "maravilha, encontramos o mike! agora precisamos voltar para o planeta terra. Você pode nos ajudar a encontrar as coordenadas?."
+    });
+
+    levelMap.set("5", {
+      level: 5,
+      text: "muito obrigado! agora estamos sãos e salvos em casa"
+    });
+
+    levelMap.set("error", {
+      text: "Parece que esse não é o número certo. Vamos tentar novamente?"
+    });
+
+    const levelObject = ref(levelMap);
+
     emits: ["changeComponent"];
 
     const changeComponent = () => {
@@ -78,16 +113,15 @@ export default {
     }
 
     onMounted(() => {
-      console.log(props.error);
 
       if (props.error) {
-        image.value = "/levels/error.gif";
+        text.value = levelObject.value.get("error").text;
       } else {
-        image.value = `/levels/level_${props.level}.gif`;
+        text.value = levelObject.value.get("" + props.level).text;
       }
     });
 
-    return { image, changeComponent };
+    return { levelObject, text, changeComponent };
   },
 };
 </script>
